7d27ea6fec9b79e189ce22d6d810cbbb6e33f614
Eric Ross <epr@rossmbox.com>
2024-03-31 20:36:24 -0700

    Use 01 examples

--- sect02/app.js
+++ sect02/app.js
@@ -1,50 +1,37 @@
 import express from 'express';
 
-const courseGoals = [];
+import { HTMX_KNOWLEDGE } from './data/htmx-info.js';
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send(`
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Learn HTMX</title>
-      <link rel="stylesheet" href="/main.css" />
-      <script src="/htmx.js" defer></script>
-    </head>
-    <body>
-      <main>
-        <h1>Manage your course goals</h1>
-        <section>
-          <form id="goal-form">
-            <div>
-              <label htmlFor="goal">Goal</label>
-              <input type="text" id="goal" name="goal" />
-            </div>
-            <button type="submit">Add goal</button>
-          </form>
-        </section>
-        <section>
-          <ul id="goals">
-          ${courseGoals.map(
-            (goal, index) => `
-            <li id="goal-${index}">
-              <span>${goal}</span>
-              <button>Remove</button>
-            </li>
-          `
-          )}
-          </ul>
-        </section>
-      </main>
-    </body>
-  </html>
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>HTMX Essentials</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+          
+        />
+        <link rel="icon" href="/icon.png" />
+        <link rel="stylesheet" href="/main.css" />
+      </head>
+      <body>
+        <header id="main-header">
+          <img src="/htmx-logo.jpg" alt="HTMX Logo" />
+          <h1>Essentials</h1>
+        </header>
+
+        <main>
+          <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
+          <button>Learn More</button>
+        </main>
+      </body>
+    </html>
   `);
 });
 
